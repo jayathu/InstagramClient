ef12fd745e57818e55521336da417c859f92346b
Jayashree Nagarajan <jayashree.nagarajan@daqri.com>
2016-02-03 15:15:20 -0800

    Added pull-to-refresh for popular stream with SwipeRefreshLayout

--- InstagramClient/app/src/main/java/project/codepath/instagramclient/PhotosActivity.java
+++ InstagramClient/app/src/main/java/project/codepath/instagramclient/PhotosActivity.java
@@ -1,7 +1,9 @@
 package project.codepath.instagramclient;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -21,6 +23,9 @@ public class PhotosActivity extends AppCompatActivity {
 
     private ArrayList<InstagramPhoto> photos;
     private InstagramPhotosAdapter aPhotos;
+    private SwipeRefreshLayout swipeContainer;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +40,77 @@ public class PhotosActivity extends AppCompatActivity {
         ListView lvPhotos = (ListView)findViewById(R.id.lvPhotos);
         lvPhotos.setAdapter(aPhotos);
         fetchPopularPhotos();
+
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchTimelineAsync(0);
+            }
+        });
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
     }
 
+
     String url = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
+
+    public void fetchTimelineAsync(int page) {
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(url, null, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+
+                //Log.i("DEBUG", response.toString());
+
+                // Remember to CLEAR OUT old items before appending in the new ones
+                aPhotos.clear();
+                JSONArray photosJSON = null;
+                try {
+                    photosJSON = response.getJSONArray("data");
+                    for (int i = 0; i < photosJSON.length(); i++) {
+                        JSONObject photoJSON = photosJSON.getJSONObject(i);
+                        //decode the attributes of the json into datamodel
+                        InstagramPhoto photo = new InstagramPhoto();
+                        photo.username = photoJSON.getJSONObject("user").getString("username");
+                        photo.caption = photoJSON.getJSONObject("caption").getString("text");
+                        photo.imageURL = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
+                        photo.likesCount = photoJSON.getJSONObject("likes").getInt("count");
+                        photo.imageHeight = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
+
+                        photos.add(photo);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                }
+
+                aPhotos.addAll(photos);
+                aPhotos.notifyDataSetChanged();
+                swipeContainer.setRefreshing(false);
+
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+            }
+        });
+    }
+
     public void fetchPopularPhotos() {
         AsyncHttpClient client = new AsyncHttpClient();
 
@@ -75,6 +148,8 @@ public class PhotosActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+               // Log.d("DEBUG", "Fetch timeline error: " + e.toString());
 
             }
         });
